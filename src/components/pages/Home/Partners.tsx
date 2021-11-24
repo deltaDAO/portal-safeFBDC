d6a90cec6853df1f96b9958eddac6a1879a05251
Luca Milanese <luca.milanese90@gmail.com>
2021-11-24 11:06:57 +0100

    fix(image imports): add query for import of partners logo

--- src/components/pages/Home/Partners.tsx
+++ src/components/pages/Home/Partners.tsx
@@ -1,26 +1,62 @@
 import React, { ReactElement } from 'react'
 import styles from './Partners.module.css'
-import deloitte from '../../../images/partners/deloitte.png'
-import sapFioneer from '../../../images/partners/sap_fioneer.png'
-import spotixx from '../../../images/partners/spotixx.png'
-import techQuartier from '../../../images/partners/tech_quartier.png'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img, { FluidObject, GatsbyImageProps } from 'gatsby-image'
 
-const partners = [
-  { logo: deloitte, website: 'https://www2.deloitte.com/de/de.html' },
-  { logo: sapFioneer, website: 'https://sapfioneer.com/' },
-  { logo: spotixx, website: 'https://spotixx.com/' },
-  { logo: techQuartier, website: 'https://techquartier.com/' }
-]
+interface partner {
+  name: string
+  website: string
+}
+
+interface logo {
+  node: { name: string; id: string; childImageSharp: GatsbyImageProps }
+}
+
+interface partnerWithLogo extends partner {
+  logo: FluidObject
+}
+
+const partnersQuery = graphql`
+  query partnersQuery {
+    partners: file(relativePath: { eq: "pages/home/partners.json" }) {
+      childHomeJson {
+        partners {
+          name
+          website
+        }
+      }
+    }
+    logos: allFile(filter: { relativeDirectory: { eq: "partners" } }) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default function Partners(): ReactElement {
+  const data = useStaticQuery(partnersQuery)
+  const partners = data.partners.childHomeJson.partners.map((e: partner) => {
+    const logo = data.logos.edges.filter((x: logo) => x.node.name === e.name)
+    return { ...e, logo: logo[0].node.childImageSharp.fluid }
+  })
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Partners</h2>
       <div className={styles.list}>
-        {partners.map((e, i) => (
+        {partners.map((e: partnerWithLogo, i: number) => (
           <div key={i} className={styles.partner}>
             <a href={e.website} target="_blank" rel="noopener noreferrer">
-              <img src={e.logo} alt="partner logo" />
+              <Img fluid={e.logo} alt="partner logo" />
             </a>
           </div>
         ))}
